1415faf72cf2d4bd9782e0f0d68aa7ea8c2a3ab5
Vladyslav Vlasenko <fl4shqq@gmail.com>
2026-07-24 20:39:37 +0300

    baseline: plain non-terragrunt provider block no default_tags

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "local" {}
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ssm_parameter" "plain" {
+  name  = "/scalrcore-39172/plain-marker"
+  type  = "String"
+  value = "plain"
+}
